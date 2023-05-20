4445c66073d5566a2b9b37774b13574393adb38a
GustavoCesias <cesiasgustavo@gmail.com>
2023-05-20 07:25:47 -0500

    Conexion a la bd y creacion de las tablas

--- src/main/java/com/gamehub/GameHubApplication.java
+++ src/main/java/com/gamehub/GameHubApplication.java
@@ -2,9 +2,8 @@ package com.gamehub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class GameHubApplication {
 
 	public static void main(String[] args) {
