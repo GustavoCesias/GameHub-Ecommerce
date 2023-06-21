3e0df4bd2148186f51e20166cd7db3ed508e26c7
GustavoCesias <cesiasgustavo@gmail.com>
2023-06-21 18:42:50 -0500

    funcionalidad mostrar productos administrador

--- src/main/java/com/gamehub/service/ProductoServiceImpl.java
+++ src/main/java/com/gamehub/service/ProductoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.gamehub.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +34,10 @@ public class ProductoServiceImpl implements ProductoService{
 	@Override
 	public void delete(Integer id) {
 		productoRepository.deleteById(id);
+	}
+	@Override
+	public List<Producto> findAll() {
+		return productoRepository.findAll();
 	}
 
 	
