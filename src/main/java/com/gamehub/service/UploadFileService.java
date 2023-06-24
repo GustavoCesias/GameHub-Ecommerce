04c5e199b9c8a1e671c00c44032097019e117c16
GustavoCesias <cesiasgustavo@gmail.com>
2023-06-23 22:06:29 -0500

    Clase de servicio para subir la imagen del producto al servidor

---
+++ src/main/java/com/gamehub/service/UploadFileService.java
@@ -0,0 +1,32 @@
+package com.gamehub.service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.File;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class UploadFileService {
+	private String folder="imagen//";
+	
+	public String saveImage(MultipartFile file) throws IOException {
+		if (!file.isEmpty()) {
+			byte [] bytes=file.getBytes();
+			Path path =Paths.get(folder+file.getOriginalFilename());
+			Files.write(path, bytes);
+			return file.getOriginalFilename();
+		}
+		return"default.jpg";
+	}
+	
+	public void deleteImage(String nombre) {
+		String ruta="images//";
+		File file= new File(ruta+nombre);
+		file.delete();
+	}
+	
+}
